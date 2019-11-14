af140c48816b0214e53e251de248efa1d380f96b
Dev Jana <devjana@Devs-MacBook-Pro-2.local>
2019-11-14 09:47:40 -0600

    get test working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,21 @@ $( document ).ready( onReady );
 
 function onReady(){
     console.log( 'JQ' );
+    testGet();
 }
+
+function testGet(){
+    // use AJAX to make a GET call to /test
+    $.ajax({
+        type: 'GET',
+        url: '/test'
+    }).then( function( response ){
+        // put response on DOM
+        console.log( 'Back from GET with:', response );
+        $( '#testOut' ).text( response );
+    }).catch( function( err ){
+        // handle any errors
+        alert( 'error getting test. see console for details' );
+        console.log( err );
+    })
+} // end testGet
--- server/server.js
+++ server/server.js
@@ -9,3 +9,9 @@ const port = 5000;
 app.listen( port, ()=>{
     console.log( 'server up on:', port );
 }) //end server up
+
+// routes
+app.get( '/test', ( req, res )=>{
+    console.log( '/test GET' );
+    res.send( 'ribbet' );
+}) //end test GET
