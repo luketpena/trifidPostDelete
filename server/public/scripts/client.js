7ebec6590d6821e68910b174556a5bf867cc9892
Dev Jana <devjana@Devs-MacBook-Pro-2.local>
2019-11-14 11:36:30 -0600

    end of lecture

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,8 +1,68 @@
 $( document ).ready( onReady );
 
 function onReady(){
-    console.log( 'JQ' );
-    testGet();
+    $( '#sendMessageButton' ).on( 'click', sendMessage );
+    $( '#refreshButton' ).on( 'click', getMessages );
+    $( '#messagesOut' ).on( 'click', '.messageLine', deleteMessage );
+    getMessages();
+}
+
+function deleteMessage(){
+    let index = $( this ).data( 'index' );
+    console.log( 'in deleteMessage, index:', index );
+    // send this ID to DELETE route
+    $.ajax({
+        type: 'DELETE',
+        url: '/messages/' + index
+    }).then( function( response ){
+        console.log( 'back from DELETE with:', response );
+    }).catch( function( err ){
+        console.log( err );
+        alert( 'no worky' );
+    })
+}
+
+function getMessages(){
+    $.ajax({
+        type: 'GET',
+        url: '/messages'
+    }).then( function( response ){
+        console.log( 'back from GET with:', response );
+        let el = $( '#messagesOut' );
+        el.empty();
+        for( let i=0; i<response.length; i++){
+            let thingy = response[ i ];
+            el.append( `<li class="messageLine" data-index="${i}"><i>${thingy.user}</i>: ${thingy.message}</li>`)
+        } // end for
+    }).catch( function( err ){
+        alert( 'error getting messages. see console for details' );
+        console.log( err );
+    })
+}
+
+function sendMessage(){
+    console.log( 'in sendMessage' );
+    // get user input & put into an object
+    let objectToSend = {
+        user: $( '#nameIn' ).val(),
+        message: $( '#messageIn' ).val()
+    } //end objectToSend
+    console.log( 'sending:', objectToSend );
+    $.ajax({
+        // send object via POST to server
+        type: 'POST',
+        url: '/messages',
+        data: objectToSend
+    }).then( function( response ){
+        // update DOM with all messages
+        console.log( 'back from POST with:', response );
+        // updateDOM
+        getMessages();
+    }).catch( function( err ){
+        // hand error
+        alert( 'error sending message. see console for details' );
+        console.log( err ); 
+    }) // end AJAX
 }
 
 function testGet(){
--- server/server.js
+++ server/server.js
@@ -9,12 +9,30 @@ app.use( express.static( 'server/public' ) );
 app.use( bodyParser.urlencoded( { extended: true } ) );
 // globals
 const port = 5000;
+let messages = [];
 // server
 app.listen( port, ()=>{
     console.log( 'server up on:', port );
 }) //end server up
 
 // routes
+app.delete( '/messages/:index', ( req, res )=>{
+    console.log( 'in /messages delete:', req.params.index );
+    res.send( 'squeak' );
+}) //end delete
+
+app.get( '/messages', ( req, res )=>{
+    console.log( 'in /messages GET' );
+    res.send( messages );
+}) // end essages
+
+app.post( '/messages', ( req, res )=>{
+    console.log( 'in /messages POST:', req.body );
+    messages.push( req.body );
+    console.log( messages );
+    res.sendStatus( 200 );
+}) // end messages POST
+
 app.get( '/test', ( req, res )=>{
     console.log( '/test GET' );
     res.send( 'ribbet' );
