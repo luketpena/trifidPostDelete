7d6925f906e36f966f39594cbba4fd84cacf373b
Luke Peña <luketpena@gmail.com>
2019-11-19 09:49:57 -0600

    updated port for Heroku

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use( express.static( 'server/public' ) );
 // also needed for POST
 app.use( bodyParser.urlencoded( { extended: true } ) );
 // globals
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // server
 app.listen( port, ()=>{
